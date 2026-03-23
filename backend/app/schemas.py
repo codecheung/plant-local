34a44d25006a7530a2a48e484429e0b092cebf1e
zhangyajun <codeff@126.com>
2026-03-24 00:26:43 +0800

    兼容 python 3.6

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,4 +1,9 @@
-from typing import List, Literal, Optional
+from typing import List, Optional
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
 
 from pydantic import BaseModel, Field
 
